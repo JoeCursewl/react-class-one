6bb12af1f7e613506c48a658e531e2ecab543eb5
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-08-09 18:38:37 -0400

    New: Upload files

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+import Navbar from './Navbar/Navbar'
+
+export default function Home() {
+
+    return (
+        <section>
+            <Navbar />
+
+            <h1>Home</h1>
+        </section>
+    )
+}
---
+++ src/pages/home/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.css'
+
+export default function Navbar() {
+
+    return (
+        <nav className='navbar-all'>
+
+            <div className='navbar'>
+                <div>
+                    <h1>LOGO</h1>
+                </div>
+
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+            </div>
+
+        </nav>        
+    )
+}

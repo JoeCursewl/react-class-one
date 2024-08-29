88207740cd6f457cb93a33f3bc4a8bf3b8ed90dd
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-08-29 16:56:29 -0400

    Added: Catalog better

--- src/pages/posters/poster-home.jsx
+++ src/pages/posters/poster-home.jsx
@@ -3,16 +3,24 @@ import './poster-home.css';
 export default function PosterHome() {
   return (
     <div className="poster-home">
-      <div>
-        <p>VENTAS</p>
+      <div className='img-e-poster-info'>
+        <img src="/posters/poster-xiaomi-img.svg" alt="" />
+        <p>Xiaomi</p>
       </div>
 
-      <div>
-        <p>COMPRADORES</p>
+      <div className='img-e-poster-info'>
+        <img src="/posters/poster-apple-img.svg" alt="" />
+        <p>Apple</p>
       </div>
 
-      <div>
-        <p>CONFIANZA</p>
+      <div className='img-e-poster-info'>
+        <img src="/posters/poster-samsung-img.svg" alt="" />
+        <p>Samsung</p>
+      </div>
+
+      <div className='img-e-poster-info'>
+        <aside>+10k</aside>
+        <p>Productos</p>
       </div>
     </div>
   );
